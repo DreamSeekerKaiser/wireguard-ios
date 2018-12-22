33d24527a931be9556b9b2be4cba3ab72715ee1b
Roopesh Chander <roop@roopc.net>
2018-12-22 18:19:59 +0530

    macOS: Add WireGuardmacOS and WireGuardNetworkExtensionmacOS targets

---
+++ WireGuard/WireGuard/UI/macOS/AppDelegate.swift
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+// Copyright © 2018 WireGuard LLC. All Rights Reserved.
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
