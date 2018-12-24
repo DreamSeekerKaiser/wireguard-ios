38808e905391c22268c91d914b8d572c3ef96742
Roopesh Chander <roop@roopc.net>
2018-12-24 16:03:05 +0530

    macOS: Create basic split-view controller setup

--- WireGuard/WireGuard/UI/macOS/AppDelegate.swift
+++ WireGuard/WireGuard/UI/macOS/AppDelegate.swift
@@ -7,10 +7,13 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
+        let mainVC = MainViewController()
+        let window = NSWindow(contentViewController: mainVC)
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        // Auto-save window position and size
+        window.windowController?.shouldCascadeWindows = false
+        window.setFrameAutosaveName(NSWindow.FrameAutosaveName("MainWindow"))
+
+        window.makeKeyAndOrderFront(self)
     }
 }
